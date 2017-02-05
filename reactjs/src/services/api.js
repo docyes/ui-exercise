90a9efc9ae00bb6e43eed18f302fbaf82386d4d8
Carl Doc Yes Yestrau <carl@splunk.com>
2017-02-05 14:15:12 -0800

    Linter for services/api package.

--- reactjs/src/services/api.js
+++ reactjs/src/services/api.js
@@ -1,19 +1,19 @@
 export default class Api {
   getWidgets() {
-    return new Promise(function (resolve) {
+    return new Promise((resolve) => {
       resolve([{
-        name:     "Standard widget",
-        size:     'medium',
-        capacity: 10
+        name: 'Standard widget',
+        size: 'medium',
+        capacity: 10,
       }, {
-        name:     "VIP widget",
-        size:     "extra-large",
-        capacity: 25
+        name: 'VIP widget',
+        size: 'extra-large',
+        capacity: 25,
       }, {
-        name:     "Starter widget",
-        size:     'small',
-        capacity: 2
-      }])
-    })
+        name: 'Starter widget',
+        size: 'small',
+        capacity: 2,
+      }]);
+    });
   }
 }
